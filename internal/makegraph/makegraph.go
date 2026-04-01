46fa1685ae6964cb288329145cbbebb9cae3369d
Bevan Arps <bevan.arps@outlook.com>
2026-04-01 13:37:20 +1300

    Add makegraph builder with tests

---
+++ internal/makegraph/makegraph.go
@@ -0,0 +1,78 @@
+package makegraph
+
+import (
+	"cmp"
+	"slices"
+
+	"github.com/theunrepentantgeek/make-graph/internal/graph"
+	"github.com/theunrepentantgeek/make-graph/internal/parser"
+)
+
+type Builder struct {
+	makefile *parser.Makefile
+}
+
+func New(mf *parser.Makefile) *Builder {
+	return &Builder{makefile: mf}
+}
+
+func (b *Builder) Build() *graph.Graph {
+	g := graph.New()
+
+	rules := make([]parser.Rule, len(b.makefile.Rules))
+	copy(rules, b.makefile.Rules)
+	slices.SortFunc(rules, func(a, b parser.Rule) int {
+		return cmp.Compare(a.Target, b.Target)
+	})
+
+	for _, rule := range rules {
+		node := g.AddNode(rule.Target)
+		node.Description = rule.Description
+	}
+
+	for _, rule := range rules {
+		b.addEdgesForPrerequisites(rule, g)
+		b.addEdgesForCalls(rule, g)
+	}
+
+	return g
+}
+
+func (b *Builder) addEdgesForPrerequisites(rule parser.Rule, g *graph.Graph) {
+	fromNode, ok := g.Node(rule.Target)
+	if !ok {
+		return
+	}
+
+	for _, prereq := range rule.Prerequisites {
+		toNode, exists := g.Node(prereq)
+		if !exists {
+			toNode = g.AddNode(prereq)
+		}
+
+		edge := fromNode.AddEdge(toNode)
+		edge.SetClass("dep")
+	}
+}
+
+func (b *Builder) addEdgesForCalls(rule parser.Rule, g *graph.Graph) {
+	fromNode, ok := g.Node(rule.Target)
+	if !ok {
+		return
+	}
+
+	for _, recipe := range rule.Recipes {
+		target, found := parser.DetectMakeCall(recipe)
+		if !found {
+			continue
+		}
+
+		toNode, exists := g.Node(target)
+		if !exists {
+			toNode = g.AddNode(target)
+		}
+
+		edge := fromNode.AddEdge(toNode)
+		edge.SetClass("call")
+	}
+}
---
+++ internal/makegraph/makegraph_test.go
@@ -0,0 +1,113 @@
+package makegraph
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/theunrepentantgeek/make-graph/internal/graph"
+	"github.com/theunrepentantgeek/make-graph/internal/parser"
+)
+
+func TestBuild_BasicRules_CreatesNodesForEachTarget(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	mf := &parser.Makefile{
+		Rules: []parser.Rule{
+			{Target: "all", Prerequisites: []string{"build", "test"}},
+			{Target: "build"},
+			{Target: "test", Prerequisites: []string{"build"}},
+		},
+	}
+
+	gr := New(mf).Build()
+
+	nodeIDs := collectNodeIDs(gr)
+	g.Expect(nodeIDs).To(ConsistOf("all", "build", "test"))
+}
+
+func TestBuild_Prerequisites_CreatesDependencyEdges(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	mf := &parser.Makefile{
+		Rules: []parser.Rule{
+			{Target: "all", Prerequisites: []string{"build", "test"}},
+			{Target: "build"},
+			{Target: "test"},
+		},
+	}
+
+	gr := New(mf).Build()
+
+	allNode, ok := gr.Node("all")
+	g.Expect(ok).To(BeTrue())
+	g.Expect(allNode.Edges()).To(HaveLen(2))
+
+	for _, edge := range allNode.Edges() {
+		g.Expect(edge.Class()).To(Equal("dep"))
+	}
+}
+
+func TestBuild_MissingPrerequisite_CreatesNodeForIt(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	mf := &parser.Makefile{
+		Rules: []parser.Rule{
+			{Target: "build", Prerequisites: []string{"main.o"}},
+		},
+	}
+
+	gr := New(mf).Build()
+
+	_, ok := gr.Node("main.o")
+	g.Expect(ok).To(BeTrue())
+}
+
+func TestBuild_RecipeWithMakeCall_CreatesCallEdge(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	mf := &parser.Makefile{
+		Rules: []parser.Rule{
+			{Target: "all", Recipes: []string{"$(MAKE) build"}},
+			{Target: "build"},
+		},
+	}
+
+	gr := New(mf).Build()
+
+	allNode, ok := gr.Node("all")
+	g.Expect(ok).To(BeTrue())
+	g.Expect(allNode.Edges()).To(HaveLen(1))
+	g.Expect(allNode.Edges()[0].Class()).To(Equal("call"))
+	g.Expect(allNode.Edges()[0].To().ID()).To(Equal("build"))
+}
+
+func TestBuild_Description_SetsNodeDescription(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	mf := &parser.Makefile{
+		Rules: []parser.Rule{
+			{Target: "build", Description: "Build the binary"},
+		},
+	}
+
+	gr := New(mf).Build()
+
+	buildNode, ok := gr.Node("build")
+	g.Expect(ok).To(BeTrue())
+	g.Expect(buildNode.Description).To(Equal("Build the binary"))
+}
+
+func collectNodeIDs(gr *graph.Graph) []string {
+	var ids []string
+	for node := range gr.Nodes() {
+		ids = append(ids, node.ID())
+	}
+
+	return ids
+}
